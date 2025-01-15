6f5767211a0034e4a3ce79a10c2f8f9c8bae1832
Mohit Rajguru <91665605+MohitRajguru@users.noreply.github.com>
2025-01-15 11:31:11 +0530

    Add files via upload

---
+++ FramesDragDrop.java
@@ -0,0 +1,26 @@
+package projects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class FramesDragDrop {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://jqueryui.com/droppable/");
+		driver.switchTo().frame(driver.findElement(By.cssSelector(".demo-frame")));
+		WebElement drag = driver.findElement(By.id("draggable"));
+		WebElement drop = driver.findElement(By.id("droppable"));
+		
+		Actions act = new Actions(driver);
+		act.dragAndDrop(drag, drop).build().perform();
+		driver.switchTo().defaultContent();
+
+	}
+
+}
